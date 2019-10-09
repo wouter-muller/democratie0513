229353cf753b190b44cf8d08adb97f3b3d5ab42e
Wouter Muller <wouter@woutermuller.net>
2019-10-09 14:56:40 +0200

    Add image zoom feature

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,3 +4,18 @@ var app = new Vue({
         hamburgerMenuOpen: false
     }
 })
+
+
+var images = document.querySelectorAll(".js-image")
+for (i = 0; i < images.length; i++) {
+  images[i].onclick = function() {
+    var snippet = this.cloneNode()
+    document.querySelector('.js-image-wrapper').classList.add('--active');
+    document.querySelector('.js-image-wrapper').appendChild(snippet);
+  };
+}
+
+document.querySelector('.js-image-wrapper').onclick = function() {
+    document.querySelector('.js-image-wrapper').classList.remove('--active');
+
+}
